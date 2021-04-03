4ea833eeb2c79504fe8107aeb77e4397c39269e7
ETamez173 <56414835+ETamez173@users.noreply.github.com>
2021-04-03 09:49:23 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// Static Methods & Classes | C# | Tutorial 30
+
+namespace CSharpExercises_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UsefulTools.SayHi("Eli");
+
+            //Console.WriteLine(Math.Sqrt(144));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UsefulTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpExercises_30
+{
+    static class UsefulTools
+    {
+
+        public static void SayHi(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+
+    }
+}
